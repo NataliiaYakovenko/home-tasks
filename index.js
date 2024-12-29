8a41f19b91e6844d7a900383fee7e607be6fddae
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-29 14:42:39 +0200

    Задаа з fotEach

--- index.js
+++ index.js
@@ -1,55 +1,29 @@
-/*Задача
-На основі задачі масива users створити новий масив users.
-При цьому об'єкти в новому масиві мають бути у такому іигляді:
-    fullname:'Nataliia Yakovenko',
-    email: 'yakovenkonatali999@gmail.com',
- */
-    const users = [{
-      name:'Nataliia',
-      lastName:'Yakovenko',
-      age: 41,
-      email: 'yakovenkonatali999@gmail.com',
-  },{
-      name:'Roland',
-      lastName:'Simonyan',
-      age: 26,
-      email: 'roland@gmail.com',
-  },{
-      name:'Bogdan',
-      lastName:'Horobriy',
-      age: 30,
-      email: 'horobriy@gmail.com',
-  }]
-  
-  //Варіант мій
-  function changeValueInObject(user){  //функція буде приймати елемент об'єкта
-      return {                         // повертаємо новий об'єкт
-          fullname: `${user.name} ${user.lastName}`,
-          email: user.email
-      }
-  }
-  
-  const newUsers = users.map(changeValueInObject)
-  console.log(newUsers);
-  //----------------------------------------------------------------------------
-  // Задача 2
-  // на основі нашої задачі зменшити вік користувачів
-  
-  function ageDecrement(user){
-      user.age -=1;
-  }
-  users.forEach(ageDecrement)
-  //-------------------------------------------------------------------------------
-  // Задача  map
-// Дано масив 
-//Зробити новий масив, де всі елементи якого = елемент зі сторого масиву + 100
-//variant 1
-const array = [2,44,11,234,8,2,4,1]
-function items(number){
-    return number + 100;
-}
-const newArray = array.map(items);
-console.log(newArray);
+// Задача 1 forEach 
+// Дано масив об'єктів
+//Всім користувачам збільшити вік на 1
 
-//variant 1 стрілочна функція
-const newArray1 = array.map ((item)=>item+100);
+const users = [{
+  name:'Nataliia',
+  lastName:'Yakovenko',
+  age: 41,
+  email: 'yakovenkonatali999@gmail.com',
+},{
+  name:'Roland',
+  lastName:'Simonyan',
+  age: 26,
+  email: 'roland@gmail.com',
+},{
+  name:'Bogdan',
+  lastName:'Horobriy',
+  age: 30,
+  email: 'horobriy@gmail.com',
+}]
+//Варіант1 мій
+users.forEach((item) => item.age += 1)
+console.log(users);
+
+//Варіант2 Рома
+function plusOneYear(item){
+  item.age +=1;
+}
+users.forEach(plusOneYear)
