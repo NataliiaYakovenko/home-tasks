b0202af32f55413ee39689a9dc43ffa1c5b65abb
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-29 14:59:41 +0200

    Задача шаблонний рядок

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-// Задача. у нас є об'єкт ключем в якому є день тижня, а значенням плаихни на цей день
-//прийняти через промпт від корстувача назву дня тижня 
-// і виадати у альорт, що заплановано у користувача на цей тиждень
-const obj = {
-  Non:'drive lessons',
-  Tue: 'dantist visit',
-  Wed: 'go to party',
-  Thu: 'hard work',
-  Fri: 'rest',
-  Sat: 'go to park',
+//Задача на шаблонні рядки
+//є функція
+/*function greetingUser (user){
 }
-const nameWeek = prompt('enter week day ( \n Monday = Mon, \n Tuesday = Tue, \n Wed, \n Thu, \n Fri, \n Sat )')
-alert(`On ${nameWeek} you have ${obj[nameWeek]}`)
+//у на є об'єкт
+const user = {
+    name: 'John',
+    surname: 'Doen',
+    age: 19,
+}*/
+//Нписати фунцію greetingUser щоб вона вітала користувача і казала скільки
+//йьому років виконається у наступному році
+//задачу вирішити через шаблонний рядок
 
-//alert(obj[nameWeek] ? obj[nameWeek] : 'Error happened') - зробити через тернальний оператор
+function greetingUser (user){
+  return (`Hello, ${user.name} ${user.surname}! You will be ${user.age + 1} next year!`)
+}
+const user = {
+  name: 'John',
+  surname: 'Doen',
+  age: 19,
+}
 
+console.log(greetingUser(user));
