d25060d627ab627810480d0671cb75ca994612ad
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-29 14:27:35 +0200

    Задача створити об'єкт газети

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
 
-'use strict' // - це директива, яку включають на початку js-файлу для встановлення
+ //ПРИКЛАД
+ //Є об'єкт газети
 
-//-------------------------------------------------------------------------
-//Задача
-//Написати стрілочну функцію, яка сумує будь - яку кількість чисел
-
-const numbers =(...restArguments)=>{  //...restArguments - вся кількість аргументів
-  let result = 0;
-  for(let i = 0; i < restArguments.length; i++ ){
-    result += restArguments[i]
+ const newspaper = {
+  titel: 'News......',
+  articles: [{
+    author: 'Nataliia Yakovenko',
+    date: '25-12-2024',
+    text: 'Lorem'
+  },{
+    author: 'Lidiia Yakovenko',
+    date: '20-04-2024',
+    text: 'Lorem'
+  },{
+    author: 'Olexander Yakovenko',
+    date: '12-10-2024',
+    text: 'Lorem'
+  }],
+  showArticles: function(){
+    this.articles.forEach ((item,index)=>{ 
+      //this.articles  - вказує на масив articles в нашом об'єкті
+      //forEach перебирає об'єкти в масиві articles
+      // в середину forEach передаємо callback
+     console.log(`${this.titel} ${index}-${item.author}`)
+    })
   }
-  return result;
+
 }
+// у об'єкта newspaper викликаємо метод showArticles
 
-console.log(numbers(1,2,3,4,5,6))
-//----------------------------------------------------------------------------
-// Написати стрілочеу функцію з використанням метода reduce
-//при виклику reduce передайте йому стрілковий callback
-const numbers1 =(...restArguments)=>{  //...restArguments - вся кількість аргументів
-  return restArguments.reduce((accumulator,curentItem) => accumulator+curentItem,0);
-  
-};
-console.log(numbers1(1,2,3,4,5,6))
-
-//варіант2
-const numbers2 =(...restArguments)=>{  
-  const sum = restArguments.reduce((accumulator,curentItem) => {
-    return accumulator+curentItem},
-  0);
-  return sum;
-};
-console.log(numbers2(1,2,3,4,5,6))
-
-//варіант3
-const numbers3 =(...restArguments)=> restArguments.reduce((accumulator,curentItem) => accumulator+curentItem,0);
-console.log(numbers3(1,2,3,4,5,6))
-
-
-
+newspaper.showArticles();
