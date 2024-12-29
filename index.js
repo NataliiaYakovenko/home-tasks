fa8c5b87c8a77137118d6fef7ffd282bf25ef3e9
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-29 15:13:08 +0200

    Задача визначити площу

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-//Зада
-/*Число r
-Якщо r > 12 - вивести в консоль більше 12
-Якщо r < 5 - вивести в консоль меньше 5
-В противному випадку - вивести в консоль діапозон між 5 і 12
- */
-//варіант 1
-/*let r = 6;
-if (r>12) {
-   console.log('більше 12');
-} else if (r<5) {
-   console.log('меньше 5');
-} else {
-   console.log('діапозон між 5 і 12');
-}*/
-
-//Варіант 2
-let r = 6;
-let strWithR = 'r= '+ r;
-if (r>12) {
-   console.log(strWithR + ', більше 12');
-} else if (r<5) {
-   console.log(strWithR + ', меньше 5');
-} else {
-   console.log(strWithR + ', діапозон між 5 та 12');
-}
-
-
+// Задача
+// У нач є ширина ідовжина прямокутника. Потрібно визначити S-?
+const length = 5;
+const wight = 8;
+const area = length * wight;
+console.log('Площа прямокутника:' + area);
