965a26cd93021f56b706598b1cb074590d28d3d2
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-29 14:24:49 +0200

    Задача на метод reduce

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 
+'use strict' // - це директива, яку включають на початку js-файлу для встановлення
+
+//-------------------------------------------------------------------------
+//Задача
+//Написати стрілочну функцію, яка сумує будь - яку кількість чисел
+
+const numbers =(...restArguments)=>{  //...restArguments - вся кількість аргументів
+  let result = 0;
+  for(let i = 0; i < restArguments.length; i++ ){
+    result += restArguments[i]
+  }
+  return result;
+}
+
+console.log(numbers(1,2,3,4,5,6))
+//----------------------------------------------------------------------------
+// Написати стрілочеу функцію з використанням метода reduce
+//при виклику reduce передайте йому стрілковий callback
+const numbers1 =(...restArguments)=>{  //...restArguments - вся кількість аргументів
+  return restArguments.reduce((accumulator,curentItem) => accumulator+curentItem,0);
+  
+};
+console.log(numbers1(1,2,3,4,5,6))
+
+//варіант2
+const numbers2 =(...restArguments)=>{  
+  const sum = restArguments.reduce((accumulator,curentItem) => {
+    return accumulator+curentItem},
+  0);
+  return sum;
+};
+console.log(numbers2(1,2,3,4,5,6))
+
+//варіант3
+const numbers3 =(...restArguments)=> restArguments.reduce((accumulator,curentItem) => accumulator+curentItem,0);
+console.log(numbers3(1,2,3,4,5,6))
 
 
-//Практика
-const array1 = [1,2,3,4,5]
-const array2 = [10,11,12,13,14,15]
-//Задача, потрібно склеїти ці два масиви
-//Не вкористовувати метод конкат
-//Використовувати ...spread - operator
 
-const arrayCopy = [...array1,...array2]
-console.log(arrayCopy);
