a39902777c58970193a7e2ba034d6a7fffa4b498
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-29 14:33:42 +0200

    Задача масив метод sort

--- index.js
+++ index.js
@@ -1,50 +1,60 @@
-// Прототипи Задача
-/* Завдання 1
-Створити функцію - конструктор для сходів (ladder)
-Об'єкт має властивість:
-curentStair - сходинка, на якій ми зараз знаходимиось. Початково = 0
+// ПРАКТИКА МАСИВИ sort
 
-Об'єкт має методи: - метоти запихнути в .prototype
-up() - підіймає на сходинку вище
-down() - опускає на сходинку нижче
-showStair() - показує на якій сходинці ми зараз знаходимось
- */
-function Labber() {
-  this.curentStair = 0;
+const dogArray = [
+  {
+    nickname: "Tuzic",
+    color: "black",
+    weght: 3,
+    age: 2,
+  },
+  {
+    nickname: "Doly",
+    color: "white",
+    weght: 5,
+    age: 3,
+  },
+  {
+    nickname: "Fagot",
+    color: "black",
+    weght: 9,
+    age: 5,
+  },
+  {
+    nickname: "Stus",
+    color: "black",
+    weght: 15,
+    age: 1,
+  },
+];
+//Задача. Відсортувати собак за вагою на збільшення у порядку зростання
+//variant1
+/*dogArray.sort((dog1, dog2) => {
+  //dog1, dog2 - два сусідніх об'єкта
+  if (dog1.weght < dog2.weght) {
+    // уразі якщо у вес у собаки1 меньше ніж у собаки2
+    return -1;
+  } else {
+    //уразі якщо у вес у собаки1 більше ніж у собаки2
+    return 1;
+  }
+});*/
+
+//variant2
+//dogArray.sort((dog1,dog2)=>{
+    //return dog1.weght - dog2.weght; //від меньшого до більшого
+    //return dog2.weght - dog1.weght; //від більшого до меншого
+//})
+//-----------------------------------------------------------------------
+// Задача. Відсортувати собак від найстаршого до молодшого
+//variant1
+/*dogArray.sort((dog1,dog2)=>{
+if(dog2.age > dog1.age){
+    return 1;
+}else {
+    return-1
 }
-
-function LabberPrototype() {
-  this.up = function () {
-    this.curentStair++;
-    return this;
-  };
-
-  this.down = function () {
-    this.curentStair--;
-    return this;
-  };
-
-  this.showStair = function () {
-    return this.curentStair;
-  };
-
-}
-
-Labber.prototype = new LabberPrototype();
-const labber = new Labber();
-
-/*console.log(labber.up())
-console.log(labber.up())
-console.log(labber.up())        //console.log для першої задачі
-console.log(labber.down())
-console.log(labber.showStair())*/
-
-/*Завдання 2
-Перепишіть методи таким чином щоб було використання ченінгу
-тобто щоб було можна об'єднати виклик методів у ланцюжок
-
-const labber = new Labber()
-Labber.up().up().down().showStair()
- */
-console.log(labber.up().up().up().down().showStair()); 
-
+})*/
+//variant2
+dogArray.sort((dog1,dog2)=>{
+return dog2.age - dog1.age;
+})
