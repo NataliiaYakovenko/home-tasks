7ecedbdd995bb82026c4af415f520cdd64969d3e
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-01 10:01:50 +0200

    Задача try...catch валідація даних

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
-//Задача
-//Написати функцію для обчислення факторіала.
-//Передбачити, щоб можна було обчислювати !
-//тільки number, прицьому цілі додатні значення
+//Практика валідація даних
+/*
+Написати функцію для валідації імені користувача (4-32, string)
+якщо значення валідне, то повернути його
+якщо ні, то згенерувати помилку
+*/
 
-function factorial(n){
-    if(typeof n !== "number"){
-        throw new Error('n must be number')
-    }
-  if(n<0 || Number.isSafeInteger(n)){  // Безпечне ціле число
-        throw new RangeError('n must be not negative and integer value')
+//validiteName(userName)=> validiteName || throw error
+// isValide => true || false
+
+function validiteName(userName) {
+  if (typeof userName !== "string") {
+    throw new TypeError("userName must be string value");
   }
- if(n === 0){
-    return 1;
- }
- return n*factorial(n-1)
+  if (userName.length < 4 || userName.length > 32) {
+    throw new RangeError("userName must be min 4 and max 32 letters");
+  }
+  return userName;
+}
+try {
+  console.log(validiteName("Nat"));
+} catch (error) {
+  console.log(error);
+}
+try {
+  console.log(validiteName('Nat'));
+} catch (error) {
+  //Як дізнатися причину помилки  instanceof
+  if(error instanceof RangeError){
+      console.log('RangeError handling');
+  }else if(error instanceof TypeError){
+      console.log('TypeError handling');
+  }else{
+    console.log('Error handling');
+  }
 
 }
-try{
-    console.log(factorial(-5));
-}catch(error){
-    console.log(err);
-}
+console.log(validiteName('Nataliia'));
+//--------------------------------------------------------------------------
+/*Реалізувати функцію, яка приймає вік,
+повертає вік, якщо параметр відповідає вимогам,
+інакше гeнeрує помилку
+ */
+function validateAge(ageUser) {
+  if (typeof ageUser !== "number") {
+    throw new TypeError("It must be number");
+  }
+  if (ageUser < 18) {
+    throw new RangeError("You must have age more 18");
+  }
 
-console.log('hello');
+  return ageUser;
+}
+try {
+    console.log(validateAge('test'));
+} catch (error) {
+    console.log(error);
+}
