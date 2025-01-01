9f1d818eb237eda5f53fabb610c2a458c9cf13d9
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-01 20:05:02 +0200

    Задача error try...catch

--- index.js
+++ index.js
@@ -1,58 +1,29 @@
-//Практика валідація даних
-/*
-Написати функцію для валідації імені користувача (4-32, string)
-якщо значення валідне, то повернути його
-якщо ні, то згенерувати помилку
-*/
-
-//validiteName(userName)=> validiteName || throw error
-// isValide => true || false
-
-function validiteName(userName) {
-  if (typeof userName !== "string") {
-    throw new TypeError("userName must be string value");
-  }
-  if (userName.length < 4 || userName.length > 32) {
-    throw new RangeError("userName must be min 4 and max 32 letters");
-  }
-  return userName;
-}
-try {
-  console.log(validiteName("Nat"));
-} catch (error) {
-  console.log(error);
-}
-try {
-  console.log(validiteName('Nat'));
-} catch (error) {
-  //Як дізнатися причину помилки  instanceof
-  if(error instanceof RangeError){
-      console.log('RangeError handling');
-  }else if(error instanceof TypeError){
-      console.log('TypeError handling');
-  }else{
-    console.log('Error handling');
-  }
-
-}
-console.log(validiteName('Nataliia'));
-//--------------------------------------------------------------------------
-/*Реалізувати функцію, яка приймає вік,
-повертає вік, якщо параметр відповідає вимогам,
-інакше гeнeрує помилку
+//Задача
+//Написати функцію сум додатніх чисел
+/**
+ * Функція знаходить суму двох додатніх чисел
+ * @param {number} a число 1
+ * @param {number} b  число 2
+ * @returns {number} сума
+ * @throws {RangeError}  якщо а || b не є цілим числом
+ * @throws {TypeError} якщо а || b не є числом
  */
-function validateAge(ageUser) {
-  if (typeof ageUser !== "number") {
+function sumPositiveNumber(a, b) {
+  if (a < 0 || b < 0) {
+    //повинні викинути помилку
+    const error = new RangeError("Any numbers are less 0");
+    throw error; // return тільки для помилок
+    console.log(error);
+  }
+  if (typeof a !== "number" || typeof b !== "number") {
     throw new TypeError("It must be number");
   }
-  if (ageUser < 18) {
-    throw new RangeError("You must have age more 18");
-  }
-
-  return ageUser;
+  return a + b;
 }
+
 try {
-    console.log(validateAge('test'));
+  //пропсуємо якийсь код, очикуючі, що тут може виникнути помилка
+  console.log(sumPositiveNumber(2, "3"));
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
