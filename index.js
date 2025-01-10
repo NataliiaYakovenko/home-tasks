24a7faeb9ab857bfe7f3e3fd56239d12ef231537
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-10 14:40:37 +0200

    Задаа Set,map повернути маив марок телефонів

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-//Повернути масив унікальних(неповторювальних) імен
+//Повернути масив марок телофонів
 
 
-const user1 =['Bob','Nataliia','Jeck','Bob', 'Jeck']
-const user2 =['Jein','Carl','Roland','Jein','Carl']
-//варіант1
-const userMain =[...new Set([...user1,...user2])]
-console.log(userMain);
-//варіант2
-const userMain1= new Set([...user1,...user2])
-const array3 = [...userMain1]
-console.log(array3);
+const phone = [
+  { name: "Test0", bhand: "Samsung" },
+  { name: "Test1", bhand: "Redmi" },
+  { name: "Test2", bhand: "Nokia" },
+  { name: "Test3", bhand: "Samsung" },
+  { name: "Test4", bhand: "Redmi" },
+  { name: "Test5", bhand: "Nokia" },
+];
+let phone1 = phone.map(phone=>phone.bhand)
+const phoneSet = new Set(phone1)
+const array4 = [...phoneSet]
+console.log(array4);
