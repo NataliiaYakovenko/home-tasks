b58932077e85555e8364f7f1fa0d4069c8deed3f
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-10 13:42:40 +0200

    Задача Set повернути масив неповторювальних значень

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-//Практика map
-//Перекладач. Словник міститься в мапі
- //1 отримати масив англомовних слів ['dog', 'run', 'white', 'cat']
-  //2 по славнику, якщо є відповідний ключ, тоді отримати для ключів значення 
-      //['собака', 'бігає', 'white', 'котом']
-      //а якщо немає тоді залишити як є
-  //3 з масива отримати рядок
-
-  const vocabulary = new Map()
-  vocabulary.set('dog','собака').set('cat','kit').set('run','бігати')
-  console.log(vocabulary);
-  
-  function translateEngToUa(engPhrase){
-    //1 отримати масив англомовних слів ['dog', 'run', 'white', 'cat']
-    const engWords = engPhrase.split(' ')
-    console.log(engWords);
-    //2 по славнику, якщо є відповідний ключ, тоді отримати для ключів значення 
-        //['собака', 'бігає', 'white', 'котом']
-        //а якщо немає тоді залишити як є
-  
-    const uaWords = engWords.map(word=>vocabulary.has(word)?vocabulary.get(word):word)    
-    console.log(uaWords);
-    //3 з масива отримати рядок
-  const uaPhrase = uaWords.join(' ')
-    return uaPhrase;
-  }
-  
-  const engPhrase = 'dog run white cat '
-  const ukrPhrase =  translateEngToUa(engPhrase)
-  console.log(ukrPhrase);
+//Задача повернути масив з унікальними (неповторювальних) значенями
+const array = [1,2,3,4,5,6,1,3,5]
+const s3 = new Set(array)
+const array2 =[...s3]
+console.log(array2);
