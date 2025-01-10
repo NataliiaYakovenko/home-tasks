d7efe5028cd496612a6c4cbeb46faa38320b45c0
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-10 15:46:47 +0200

    Задача set вибрати name до кожного бренда телефону

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-//Повернути масив марок телофонів
 
-
-const phone = [
-  { name: "Test0", bhand: "Samsung" },
-  { name: "Test1", bhand: "Redmi" },
-  { name: "Test2", bhand: "Nokia" },
-  { name: "Test3", bhand: "Samsung" },
-  { name: "Test4", bhand: "Redmi" },
-  { name: "Test5", bhand: "Nokia" },
+//Повернути name які належать окремим маркам телефонів
+//Samsung:["Test0","Test3"]
+//Redmi:["Test1","Test4"]
+//Nokia:["Test2","Test6"]
+const phone2 = [
+  { name: "Test0", brand: "Samsung" },
+  { name: "Test1", brand: "Redmi" },
+  { name: "Test2", brand: "Nokia" },
+  { name: "Test3", brand: "Samsung" },
+  { name: "Test4", brand: "Redmi" },
+  { name: "Test5", brand: "Nokia" },
 ];
-let phone1 = phone.map(phone=>phone.bhand)
-const phoneSet = new Set(phone1)
-const array4 = [...phoneSet]
-console.log(array4);
+
+let phone3 = phone2.map(phone2=>phone2.brand)
+const phoneSet1 = new Set(phone3)
+const array5 = [...phoneSet1]
+console.log(array5);
+
+//const usersPhone ={}
+// array5.forEach(b=>(usersPhone[b]=[]))
+//console.log(usersPhone);
+//отримати всі об'єкти де бренд Nokia
+//const userWithNokia = phone2.filter(u=>u.brand === 'Nokia')
+//console.log(userWithNokia);
+//const usersNameWithNokia = userWithNokia.map(u=>u.name)
+//console.log(usersNameWithNokia);
+
+  //отримати всі об'єкти де бренд Nokia
+
+  const phoneWithUsers ={}
+  array5.forEach(
+      b=>(phoneWithUsers[b]=phone2.filter(u=>u.brand === b).map(u=>u.name)))
+ console.log(phoneWithUsers);
