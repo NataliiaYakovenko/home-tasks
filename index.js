dd57d0db4dbf119ad450a75a07d30846c990ecd6
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-10 11:19:28 +0200

    задача Map Словник перекладач

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
-class MyArray {
-    constructor() {
-      this.length = 0; // довжина нашого масиву завжди буде дорівнюати 0 при створені масиву
-    }
-    push() {
-      for (let i = 0; i < arguments.length; i++) {
-        this[this.length] = arguments[i]; // в поточну довжину запихуємо значення
-        this.length++; //потрібно збільшити довжуну масива на 1
-      }
-      return this.length; //повертаємо поточну довжину масива
-    }
+//Практика map
+//Перекладач. Словник міститься в мапі
+ //1 отримати масив англомовних слів ['dog', 'run', 'white', 'cat']
+  //2 по славнику, якщо є відповідний ключ, тоді отримати для ключів значення 
+      //['собака', 'бігає', 'white', 'котом']
+      //а якщо немає тоді залишити як є
+  //3 з масива отримати рядок
+
+  const vocabulary = new Map()
+  vocabulary.set('dog','собака').set('cat','kit').set('run','бігати')
+  console.log(vocabulary);
   
-    pop() {
-      if (this.length > 0) {
-        //якщо довжина масив більше 0
-        //1. Зберегти останній елемент
-        const lastItem = this[this.length - 1]; //потрібно від поточної довжини масива - 1
-        //2.Видалити останній елемент з масиву
-        delete this[this.length - 1];
-        //3.Після видалення останнього елементу нам потрібно зменшити довжину масиву на 1
-        this.length--;
-        //4. Повернути останній елемент
-        return lastItem;
-      } else {
-        return undefined;
-      }
-    }
+  function translateEngToUa(engPhrase){
+    //1 отримати масив англомовних слів ['dog', 'run', 'white', 'cat']
+    const engWords = engPhrase.split(' ')
+    console.log(engWords);
+    //2 по славнику, якщо є відповідний ключ, тоді отримати для ключів значення 
+        //['собака', 'бігає', 'white', 'котом']
+        //а якщо немає тоді залишити як є
   
-    forEach(callback) {
-      for (let i = 0; i < this.length; i++) {
-        callback(this[i], i, this);
-      }
-    }
+    const uaWords = engWords.map(word=>vocabulary.has(word)?vocabulary.get(word):word)    
+    console.log(uaWords);
+    //3 з масива отримати рядок
+  const uaPhrase = uaWords.join(' ')
+    return uaPhrase;
   }
   
-  const array1 = new MyArray()
-  console.log(array1.push(1,2,3,4,5));
-  console.log(array1)
-  console.log(array1.pop());
-  console.log(array1)
-  array1.forEach((item)=>{
-    console.log(item**2)});
-  
-
+  const engPhrase = 'dog run white cat '
+  const ukrPhrase =  translateEngToUa(engPhrase)
+  console.log(ukrPhrase);
