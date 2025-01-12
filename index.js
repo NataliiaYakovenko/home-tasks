3c9b5251c9bfdb25d5b5a56c47e779f2eb4143d4
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-12 14:38:12 +0200

    Задача Алгоритм. Лінійні, Квадратичні, Логарифмічні функції

--- index.js
+++ index.js
@@ -1,69 +1,80 @@
-//Класи перепризначення методів
+//Алгоритм
+
+const array = [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 8, 7, 6, 5, 4, 3];
+//Задача. Реалізувати функцію лінійного пошуку якогось значення в масиві
+
+//Лінійна складність алгоритму
+function leanerSearch(array, value) {
+  //array- наш масив,value - значення в масиві, яке ми хочемо знайти
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      //якщо значення нашого маисву = value
+      return i; // повертаємо це значення
+    }
+  }
+  return -1; //якщо нашого значення в масиві не має.-1 означає,що такого значення не існує
+}
+console.log(leanerSearch(array, 9));
+
 /*
-squirrel(білка): має ім'я, їсть, що дадуть, бігає
-flaingSquirrel(літаюча білка): має розмах крильєв, їсть тільки горіхи,літає
+Лінійна складність (найгірше), коли Big O, тобто коли стільки ітерацій (операцій),
+скільки в нас вхідних даних. Поки ми не пройдемо всі значення в масиві
 */
-class Squirrel {
-  constructor(nickname) {
-    this.nickname = nickname;
-  }
-  set nickname(value) {
-    if (typeof value !== "string") {
-      throw new TypeError("Nickname must be a string.");
+//-----------------------------------------------------------------------------------------
+
+//Квадратична складність
+//Вивести таблицю множення
+function multiTable(limit) {//limit - до якого числа ми будемо друкувати таблицю множення
+  const table = []; //таблиця множення буде масивом
+  for (let i = 1; i <= limit; i++) { //починаємо таблицю множення з 1
+    for (let j = 1; j <= limit; j++) {//другий цикл на що ми будемо множити
+    const number = i * j;
+    table.push(`${i} * ${j} = ${number}`)
     }
-    if (value === " ") {
-      throw new RangeError("Nickname cannot be empty.");
-    }
-    this._nickname = value;
   }
-  get nickname() {
-    return this._nickname;
-  }
-  eating(food) {
-    console.log(`Squirrel ${this._nickname} eats ${food}.`);
-  }
-  runing() {
-    console.log(`Squirrel ${this._nickname} runs.`);
-  }
+  return table;
 }
-class FlaingSquirrel extends Squirrel {
-  constructor(nickname, wingSpan) {
-    super(nickname);
-    this.wingSpan = wingSpan;
-  }
-  set wingSpan(value) {
-    if (value === " ") {
-      throw new RangeError("Wing span cannot be empty.");
-    }
-    if (typeof value !== "number") {
-      throw new TypeError("Wing span must be a number.");
-    }
-    this._wingSpan = value;
-  }
+console.log(multiTable(1)); //['1 * 1 = 1']
+console.log(multiTable(2)); //(4) ['1 * 1 = 1', '1 * 2 = 2', '2 * 1 = 2', '2 * 2 = 4']
+console.log(multiTable(3)); //(9) ['1 * 1 = 1', '1 * 2 = 2', '1 * 3 = 3', '2 * 1 = 2', '2 * 2 = 4', '2 * 3 = 6', '3 * 1 = 3', '3 * 2 = 6', '3 * 3 = 9']
+console.log(multiTable(10));
 
-  get wingSpan() {
-    return this._wingSpan;
-  }
+//Подвійні цикли - це є квадратична складність
+/*
+Квадаратична складність, коли ми на виході отримуємо 
+кількість вхідних даних (n) в квадраті
+*/
+//----------------------------------------------------------------------------------------------------
 
-  eating() {
-    console.log(`Flaing squirrel ${this._nickname} eats only nuts.`);
-  }
-  runing() {
-    console.log(`Flaing squirrel ${this._nickname} cannot run.`);
-  }
-  
-  flaing(){
-    console.log(`Flaing squirrel ${this._nickname} flaes and has length wind span ${this.wingSpan}`);
-  }
+//Логарифмічна складність
+//На 100 вхідних даних даних буде приблизно 6 ітерацій(операцій)
+
+//стоврюємо відсартирований масив по зростанню
+//якщо не буде відстортування логарифм працювати не буде
+const array2 = [2,3,4,5,6,7,8,9,10]
+
+// потрібне доопрацювання
+function binorySearch(array,whatToFinde){ //array - приймаємо якийсть масив,whatToFinde - те,що ми шукаємо
+//перевіряємо чи є внаявності наше шукаєме значення
+if(whatToFinde > array[array.length-1]){  // якщо наш шукаєме значення більше ніж довжина масива
+return -Infinity  // це позначка, що такого значення вмаисві  не має
 }
-
-const squirrel = new Squirrel("Bamby");
-console.log(squirrel);
-squirrel.eating('strawberry');
-squirrel.runing();
-
-const flaingSquirrel = new FlaingSquirrel('Leyla',29)
-console.log(flaingSquirrel);
-flaingSquirrel.eating();
-flaingSquirrel.runing();
-flaingSquirrel.flaing();
+let start = 0 ; // початковий індекс масиву
+let end = array.length-1; //кінцевий індекс масиву
+let middle = Math.round((start + end) /2); //середній індекс масиву
+                                          //Math.round - округлює десятичну дробь(20.3 буде 20, 20.7 буде 21)
+while(true){
+    if(array[middle]===whatToFinde){ //якщо середній елемент такий, який ми шукаємо
+        return middle;
+    }
+    if(array[middle] < whatToFinde){  //якщо середній елемент менше шукаємого
+        start = middle;
+        middle = Math.ceil((start+end)/2); //Math.ceil - округлює в більшу сторону
+    }else{  //якщо середній елемент більше шукаємого
+        end = middle;
+        middle = Math.ceil((start+end)/2);//Math.ceil - округлює в більшу сторону
+    }
+}
+}
+console.log(array2)//(9) [2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(binorySearch(array2,6)); //4
