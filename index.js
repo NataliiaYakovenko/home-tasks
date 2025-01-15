4e2a49f74b89f459122215ffeaf97fe2112059b9
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-15 23:32:30 +0200

    Задача Map Створюємо слоаник

--- index.js
+++ index.js
@@ -1,45 +1,27 @@
-//Реалізація стека
-class Stack {
-  constructor(maxSize) {
-    this._maxSize = maxSize;
-    this._size = 0;
-  }
+//Map     Створюємо словник
+const vocabulary = new Map();
+vocabulary.set("cat", "кіт");
+vocabulary.set("dog", "собака");
+vocabulary.set("eat", "їсти");
 
-  get isEmpty() {
-    return this._size === 0; // якщо розмір = 0 тоді повертається true/ (оператор === повертає булєвоє значення)
-  }
-  set size(newValue) {
-    throw new Error("Access denied"); // доступ заборонено
-  }
-  get size() {
-    return this._size;
-  }
-  set maxSize(newValue) {
-    throw new Error("Access denied"); // доступ заборонено
-  }
-  get maxSize() {
-    return this._maxSize;
-  }
-  push(newValue) {
-    if (this._size > this._maxSize) {
-      throw new Error("Stack overflow");
+/*
+Написати функцію, яка приймає строку англійською
+і перекладає цю строку українською використовуючи наш словник
+*/
+function translate(string, vocabulary) {
+  const arrayWords = string.toLowerCase().trim().split(" ");
+  console.log(arrayWords);
+  //у нас є словник vocabulary
+  //перебрати arrayWords і переклад для кожного англійського слова у масиві
+  const translatedArray = arrayWords.map((word) => {
+    if(vocabulary.has(word)){
+      return vocabulary.get(word);
+    }else{
+      return word;
     }
-    this[`_${this.size}`] = newValue;
-    this._size++;
-    return this.size;
-  }
-  pop() {
-    if (this.size > 0);
-    {
-      const lastitem = this[`_${this.size - 1}`];
-      delete this[`_${this.size}`];
-      this._size--;
-
-      return lastitem;
-    }
-  }
-  pick() {
-    return this[`_${this.size-1}`];
-  }
+  });
+  return translatedArray.join(' ');
+  //дістати з нашого слованика vocabulary
 }
-const myStack = new Stack(4)
+console.log(translate("      Cat eat dog             ", vocabulary))
+console.log(translate('Dog eat cat',vocabulary));
