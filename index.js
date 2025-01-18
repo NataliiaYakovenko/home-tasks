1dd0b32e9a6563361637e5bfc6b70ff8a1c69c81
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-18 21:44:36 +0200

    Задача приклади замикання

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
-// Деструктуризація вхідних параметрів
-//Деструктуризувати firstName,lastName
-
-function getFullName({firstName,lastName,...restObject}){           //деструктуризуємо, що нам потрібно
-    console.log(restObject)
-    return `${firstName} ${lastName}`
-  };
-  
-  const user ={
-     firstName: 'Nataliia',
-     lastName: 'Yakovenko',
-     age:41,
-     geolocation: '45.56677555.433.222222',
-     browser: 'Ubuntu'
+//Буде повертати значення і збільшене на 1
+function makeCounter() {
+    let i = 0;
+    return function () {
+      return i++; // i - це змінна у замиканні
+    };
   }
+  const fn = makeCounter();
+  console.log(fn()); //0
+  console.log(fn()); //1
+  console.log(fn()); //2
+  console.log(fn()); //3
+  console.log(fn()); //4
+  //---------------------------------------
   
-  console.log(getFullName(user));
+  function makeCounter2() {
+    let counter = 0;
+  
+    return {
+      increment() {
+        return ++counter;
+      },
+      dicrement() {
+        return --counter;
+      },
+    };
+  }
+  const fnObj = makeCounter2();
+  console.log(fnObj); //{increment: ƒ, dicrement: ƒ}
+  
+  console.log(fnObj.increment()); //1
+  console.log(fnObj.increment()); //2
+  console.log(fnObj.increment()); //3
+  
+  console.log(fnObj.dicrement()); //2
+  console.log(fnObj.dicrement()); //1
+  console.log(fnObj.dicrement()); //0
+  //------------------------------------------------------
+  //Зробіть методи, які будуть збільшувати,зменшувати counter на певну кількість
+  
+  function makeCounter3() {
+    let counter = 0;
+  
+    return {
+      increment(amount = 1) {
+        return (counter += amount);
+      },
+      decrement(amount = 1) {
+        return (counter -= amount);
+      },
+    };
+  }
+  const fnObj3 = makeCounter3();
+  console.log(fnObj3.increment(2));
+  console.log(fnObj3.decrement(1));
+  console.log(fnObj3.increment(3));
+  console.log(fnObj3.decrement(1));
+  
   
