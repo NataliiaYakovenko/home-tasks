99670ab0b87bfeac3e9e32e6044ee39df7209261
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-18 18:10:43 +0200

    Задача Деструктуризація масива

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 
-//Задача Деструктуризація
-//Дестректурузувати масив з об'єкта
-//Задача 2
-const user={
-    name: 'Nataliia',
-    age: 41,
-    adress:{
-      city:'Zaporizhzha',
-      country:'Ukraine',
-    },
-    contacts:{
-      email: 'yakovenkonatali@gmail.com',
-      phone:[+380662865135,+380997865534,+380674532412],
-  
-    }
-  }
-  const{contacts:{phone:[firstNumber,secondNumber,thirdNumber]}}=user
-  
-  //const[firstNumber,secondNumber,thirdNumber]=phone;
-  console.log(firstNumber);
-  console.log(secondNumber);
-  console.log(thirdNumber);
+//Деструктуризація масивів
+//Масиви надають читкий порядок елементів
+const arr =[1,2,3,4,5,6];
+
+//const firstElement = arr[0]; //- виводимо перший елемент
+//виводимо перший елемент через деструктуризацію масива
+//const[firstElement,secondElement,thirdElement,fourthElement]=arr;
+
+//Якщо нам потрібно тільки перші два елементи масиву
+const[firstElement,secondElement,...restOfArr]=arr;
   
