f3b06524f2cd15880db06dbb7149ae69db7bb6f9
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-18 17:11:50 +0200

    Задача Деструктуризація

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-// Set - задачі
 
-/*
-Дано два масиви:
-const array1 = [4,3,7,5,-11]
-const array2 =[3,4,8,7,2,-11]
-Повернути з функції масив, що складається з різниці двох масивів
-(тобто тих значень, які не повторюються в обох масивах)
-
-Алгоритм
-Ви приймаєте два маисиви
-Васм потрібно зробити новий масив, але там неповинно бути дублювань
-*/
-//varius1
-const array1 = [4,3,7,5,-11]
-const array2 =[3,4,8,7,2,-11]
-const arraySet = [...array1,...array2];
-console.log(arraySet);
-const set = new Set(arraySet)
-console.log(set)
-const arr = Array.from(set)
-console.log(arr);
-
-//varius3 with function
-function twoArraysWithoutDoubles(array1,array2){
-  return [...new Set([...array1,...array2])]
-}
-const arrayWithout = twoArraysWithoutDoubles(array1,array2)
-console.log(arrayWithout)
-
+//Задача Деструктуризація
+//У нас є об'єкт user
+//З використанням деструктуризації отримати значення name,city, email,phone
+const user={
+    name: 'Nataliia',
+    age: 41,
+    adress:{
+      city:'Zaporizhzha',
+      country:'Ukraine',
+    },
+    contacts:{
+      email: 'yakovenkonatali@gmail.com',
+      phone:'+30662865135',
+  
+    }
+  }
+  const{name,adress:{city},contacts:{email,phone}}=user;
+  console.log(name)
+  console.log(city);
+  console.log(email);
+  console.log(phone);
