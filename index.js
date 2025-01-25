28832521129abcd6085e8ca02055349aa9be9f9d
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-25 11:49:01 +0200

    Конвертер з градусів Цельсію на Фарингейти

--- index.js
+++ index.js
@@ -1,54 +1,19 @@
-/*
-Створити конвертер валют
+//Задача Зробити конвертер з градусів Цельсію на Фарингейти
+//Формула
+//(Температура в градусах Цельсія * 9/5) + 32
 
-Декомпозиція - це вирішення задачі частинами:
-1.Маємо створити HTML сторінку з формою, яка містить два поля вводу
- - одне поле для ведення суми в одній валюті
- - інше поле - для вибору валюти, в яку ми будемо цю суму конвертувати
+const degrees = document.querySelector("#degrees-сelsius");
+
+function fahrenheit(event) {
+  event.preventDefault();
+
+  const temperature = Number(document.querySelector("#temperature").value);
  
-2. Додати в форму кнопку 'Конвертувати' 
-
-3. Написати JS обробник події відправки форми
- - отримати значення введеної суми та обраної валюти
- - обчислити конвертовану суму, помноживши введену користувачем суму
- на курс обміну вибраної користувачем валюти
- - вивести результат конвертації на сторінку для відображеня результату (textContent)
-
-4. Підписати форму на подію відправки форми 'submit', 
-в якості слухача використовувати функцію з пункта 3  
-
-*/
-//-----------------------------------------------------------------
-//створюємо об'єкт для зберігання курсу валют
-const CURRENCY = {
-  USD_TO_UAH: 37.65,
-  EUR_TO_UAH: 40.61
+  //1°C = (1°C*9/5) + 32 = 33.8
+  const fahrenheitDegrees = (temperature * 9 / 5)  + 32;
+;
+  const result = document.querySelector("#result");
+  result.innerHTML = `${temperature}°C = ${fahrenheitDegrees.toFixed(2)}°F`;
 }
-const convertorForm = document.querySelector('#currency-converter-form');
 
-
-function convertHandler(event){
-    event.preventDefault() // метод, який зупинить перезавантаження сторінки
-  
-    //отримуємо значення введеної суми та обраної валюти
-    //число повертається  string, тому використовуємо конструктор Number
-    const amount = Number(document.querySelector('#amount').value);
-    const currency = document.querySelector('#currency').value;
-    
-    let convertAmount; // сюди покладемо конвертовану суму
-    //обчислюємо конвертовану суму
-    if(currency === 'USD'){
-        convertAmount = amount * CURRENCY.USD_TO_UAH //число помножуємо на курс долора
-    } else if(currency === 'EUR'){
-        convertAmount = amount * CURRENCY.EUR_TO_UAH //число помножуємо на курс евро
-    } else{
-        throw new Error('Something problem ...!')
-    }
-
-    //вивводимо результат конвертації на сторінку
-    const result = document.querySelector('#result')
-
-    result.innerHTML = `${amount} ${currency} = ${convertAmount.toFixed(2)} UAH`
-}
-//Підписуємо форму на подію відправки форми 'submit'
-convertorForm.addEventListener('submit', convertHandler);
+degrees.addEventListener("submit", fahrenheit);
