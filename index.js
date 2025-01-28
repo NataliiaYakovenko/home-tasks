c7b5948dcf2f9b39b6988b3d57ff76735a4f876e
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-28 20:57:40 +0200

    Лічильник

--- index.js
+++ index.js
@@ -1,49 +1,26 @@
-const slides = [
-  {
-    src: 'https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    alt: 'landscape1',
-  },
-  {
-    src: 'https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    alt: 'landscape2',
-  },
-  {
-    src: 'https://images.pexels.com/photos/371589/pexels-photo-371589.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    alt: 'landscape3',
-  },
-  {
-    src: 'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    alt: 'landscape4',
-  },
-];
- 
-let currentSlideIndex = 0;
+/*Алгоритм 
+1. Отримати посилання на кнопки
+2. Створити змінну для counter(лічильник)
+3. Навісити обробник на кнопку(-)
+4. Навісити обробник на кнопку(+)
+5. В обробнику зменшити counter(лічильник)
+*/
+const [decBtn, incBtn] = document.querySelectorAll(".counterBtn");
+const counterElement = document.querySelector('#counter')
 
-//встановити в якості початкового зображення slides[0]
-const slideImage = document.querySelector('img');
-slideImage.src = slides[currentSlideIndex].src;
-slideImage.alt = slides[currentSlideIndex].alt;
+let counter = 0;
+counterElement.textContent =counter;
 
-const [prevBtn, nextBtn] = document.querySelectorAll('.navBtn');
-
-nextBtn.addEventListener('click', nextSlideHandler);
-function nextSlideHandler(event){
-  if(currentSlideIndex < slides.length-1){
-      currentSlideIndex++
-  }else{
-      currentSlideIndex = 0;
-  }
-  slideImage.src = slides[currentSlideIndex].src;
-  slideImage.alt = slides[currentSlideIndex].alt;
+decBtn.addEventListener("click", decrementCount);
+function decrementCount(event) {
+  counter--;
+  counterElement.textContent = counter;
 }
 
-prevBtn.addEventListener('click',prevSlideHandler)
-function prevSlideHandler(event){
-  if(currentSlideIndex > 0){
-      currentSlideIndex--;
-  }else{
-      currentSlideIndex = slides.length - 1;
-  }
-  slideImage.src = slides[currentSlideIndex].src;
-  slideImage.alt = slides[currentSlideIndex].alt;
+incBtn.addEventListener("click", incrementCount);
+function incrementCount(event) {
+  counter++;
+  counterElement.textContent = counter;
 }
+
+
