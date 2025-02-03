8ed55ac3db9af6e1b04da5a6d2f373212263f907
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-03 22:46:41 +0200

    Гра квадратик переміщувати по полю

--- index.js
+++ index.js
@@ -1,62 +1,26 @@
 "use strict";
-
-const user = {
-  firstName: "Roland",
-  lastName: "Simonyan",
-  age: 27,
-  photoSrc:
-    "https://img.freepik.com/free-psd/3d-illustration-person-with-long-hair_23-2149436197.jpg?ga=GA1.1.1872930788.1728835828&semt=ais_hybrid",
-};
 /*
-Алгоритм
+Дана section, на якій ми будемо клацати мишею
+Дан div, який має переміститись на точку, в яку клцнули мишею
 
-Обовєязкові
-1. Створити елемент
-
-Необхідні
-2. Задати значення атрибута
-3. Якщо необхідно.Додати класи
-4. Додати контент
-5. Додати обробник на якусь подію
-
-Обовєязкові
-6. Вбудуваи його туди куди потрібно
 */
-const userCardEl = document.createElement('article');
-userCardEl.classList.add('user-card')
-document.body.append(userCardEl);
 
-const userImg = document.createElement('img');
-userImg.src = user.photoSrc;
-userImg.alt = `${user.firstName} ${user.lastName}`;
-userImg.classList.add('user-image');
-userCardEl.append(userImg);
+const gameField = document.querySelector("#game-field");
+const box = document.querySelector("#box");
 
-const userInfo = document.createElement('div');
-userInfo.classList.add('user-info');
-userCardEl.append(userInfo);
+gameField.addEventListener("click", clickHandler,{capture:true});
 
-const userNameEl = document.createElement('p');
-userNameEl.classList.add('user-name');
-userNameEl.textContent = `${user.firstName} ${user.lastName}`;
-userInfo.append(userNameEl);
+function clickHandler(event) {
+  //Якщо ми натискаємо не ігрове поле(тобто на квадратик) - переміщеню не бути
+  event.stopPropagation();
+  //target - це елемент, на якому відбулась подія (на який елемент саме клікнули)
+  //currentTarget - якому елементу належав обробник подій
 
-const userAge = document.createElement('span');
-userAge.classList.add('user-age');
-userAge.textContent = user.age;
-userInfo.append(userAge);
+   //Якщо ми натискаємо на ігрове поле - переміщеню бути
+   if (event.currentTarget === event.target) { 
+    const {target:{children: { box }},clientY,clientX} = event;
 
-const trash = document.createElement('i');
-trash.classList.add('fa-solid');
-trash.classList.add('fa-trash');
-trash.classList.add('trash-icon');
-
-function deleteTfash(event){
-  userCardEl.remove();
+    box.style.top = `${clientY - (box.offsetHeight)}px`;
+    box.style.left = `${clientX - (box.offsetWidth)}px`;
+   }
 }
-trash.onclick = deleteTfash;
-
-userCardEl.append(trash);
-
-
-
