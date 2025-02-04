7909b1dc25401be4c741a8a69cd6fc5905bf8344
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-04 22:10:27 +0200

    Створення Users card over DOM

--- index.js
+++ index.js
@@ -1,26 +1,102 @@
 "use strict";
-/*
-Дана section, на якій ми будемо клацати мишею
-Дан div, який має переміститись на точку, в яку клцнули мишею
 
-*/
+/* <article class="card-wrapper">
+        <img class="card-image" src="https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg" 
+        alt="photo Nataliia">
+         <h2 class="username">Nataliia</h2>
+         <p class="description">Descroption for Nataliia</p>
+      </article> */
+const data = [
+  {
+    id: 1,
+    name: "Nataliia",
+    descroption: "Descroption for Nataliia",
+    profilePicture:
+      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
+  },
+  {
+    id: 2,
+    name: "Roland",
+    descroption: "Descroption for Roland",
+    profilePicture:
+      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
+  },
+  {
+    id: 3,
+    name: "David",
+    descroption: "Descroption for David",
+    profilePicture:
+      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
+  },
+  {
+    id: 4,
+    name: "Jeck",
+    descroption: "Descroption for Jack",
+    profilePicture:
+      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
+  },
+  {
+    id: 5,
+    name: "Susan",
+    descroption: "Descroption for Susan",
+    profilePicture:
+      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
+  },
+  {
+    id: 6,
+    name: "Oscar",
+    descroption: "Descroption for Oscar",
+    profilePicture:
+      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
+  },
+  {
+    id: 7,
+    name: "Lilu",
+    descroption: "Descroption for Lilu",
+    profilePicture:
+      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
+  },
+  {
+    id: 6,
+    name: "For",
+    descroption: "Descroption for For",
+    profilePicture:
+      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
+  },
+];
 
-const gameField = document.querySelector("#game-field");
-const box = document.querySelector("#box");
+const root = document.querySelector("#root");
 
-gameField.addEventListener("click", clickHandler,{capture:true});
+function creatUserCard(user) {
+  //Створення article
+  const article = document.createElement("article");
+  article.classList.add("card-wrapper");
 
-function clickHandler(event) {
-  //Якщо ми натискаємо не ігрове поле(тобто на квадратик) - переміщеню не бути
-  event.stopPropagation();
-  //target - це елемент, на якому відбулась подія (на який елемент саме клікнули)
-  //currentTarget - якому елементу належав обробник подій
+  //Створення img
+  const img = document.createElement("img");
+  img.setAttribute("src", user.profilePicture);
+  img.setAttribute("alt", user.name);
+  img.classList.add("card-image");
 
-   //Якщо ми натискаємо на ігрове поле - переміщеню бути
-   if (event.currentTarget === event.target) { 
-    const {target:{children: { box }},clientY,clientX} = event;
+  //Створення h2
+  const h2 = document.createElement("h2");
+  h2.append(user.name);
+  h2.classList.add("username");
 
-    box.style.top = `${clientY - (box.offsetHeight)}px`;
-    box.style.left = `${clientX - (box.offsetWidth)}px`;
-   }
+  //Створення параграфу
+  const p = document.createElement("p");
+  p.append(user.description);
+  p.classList.add("description");
+
+  //Прєднати до article елементи створені в пунктах 2-4 (img,h2,p)
+  article.append(img, h2, p);
+
+  //Повертаємо створений article
+  return article;
 }
+
+console.log(data);
+const cardArray = data.map((user) => {
+  return creatUserCard(user);
+});
+root.append(...cardArray);
