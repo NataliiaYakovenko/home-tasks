43617b4d21c9e5146c49ac7fb505838691a299cc
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-08 06:53:16 +0200

    Todo list

--- index.js
+++ index.js
@@ -1,102 +1,59 @@
 "use strict";
 
-/* <article class="card-wrapper">
-        <img class="card-image" src="https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg" 
-        alt="photo Nataliia">
-         <h2 class="username">Nataliia</h2>
-         <p class="description">Descroption for Nataliia</p>
-      </article> */
-const data = [
-  {
-    id: 1,
-    name: "Nataliia",
-    descroption: "Descroption for Nataliia",
-    profilePicture:
-      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
-  },
-  {
-    id: 2,
-    name: "Roland",
-    descroption: "Descroption for Roland",
-    profilePicture:
-      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
-  },
-  {
-    id: 3,
-    name: "David",
-    descroption: "Descroption for David",
-    profilePicture:
-      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
-  },
-  {
-    id: 4,
-    name: "Jeck",
-    descroption: "Descroption for Jack",
-    profilePicture:
-      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
-  },
-  {
-    id: 5,
-    name: "Susan",
-    descroption: "Descroption for Susan",
-    profilePicture:
-      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
-  },
-  {
-    id: 6,
-    name: "Oscar",
-    descroption: "Descroption for Oscar",
-    profilePicture:
-      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
-  },
-  {
-    id: 7,
-    name: "Lilu",
-    descroption: "Descroption for Lilu",
-    profilePicture:
-      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
-  },
-  {
-    id: 6,
-    name: "For",
-    descroption: "Descroption for For",
-    profilePicture:
-      "https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg",
-  },
-];
+/*Задача. Створення простенької TODO
+Базовий рівень
+1. Зробити input для вводу тексту
+2. Зробити кнопку, яка по натисненю, текст з інпута перетворює на елемент списку
 
-const root = document.querySelector("#root");
+     <form>
+      <input type="text"/>
+      <button>Click to add</button>
+     </form>
+     <ul>
+      <li>Зробити щось одне</li>
+      <li>Зробити щось друге</li>
+      <li>Зробити щось третє</li>
+     </ul>
 
-function creatUserCard(user) {
-  //Створення article
-  const article = document.createElement("article");
-  article.classList.add("card-wrapper");
 
-  //Створення img
-  const img = document.createElement("img");
-  img.setAttribute("src", user.profilePicture);
-  img.setAttribute("alt", user.name);
-  img.classList.add("card-image");
+  Просунутий рівень
+  Задача до кожної li приєднати кнопку за допомогою якої елемент зі списку можна видалити
+ */
 
-  //Створення h2
-  const h2 = document.createElement("h2");
-  h2.append(user.name);
-  h2.classList.add("username");
+const form = document.querySelector("#todo-form");
+let taskCount = 0;
 
-  //Створення параграфу
-  const p = document.createElement("p");
-  p.append(user.description);
-  p.classList.add("description");
+form.addEventListener("submit", addItem);
+function addItem(event) {
+  event.preventDefault();//щоб постійно не оновлювалась сторінка
 
-  //Прєднати до article елементи створені в пунктах 2-4 (img,h2,p)
-  article.append(img, h2, p);
+  //Перевірка чи не забагато завдань на сьогодні
+  if(taskCount === 10){
+   alert('We have max tasks')
+   return;
+  }
 
-  //Повертаємо створений article
-  return article;
+  const { target } = event; //деструктуризуємо подію
+  const [todoInput] = target; //деструктуризуємо мету в події (інпут)
+
+  //Відбувається валідація інпута
+  const value = todoInput.value.trim()  //trim()видаляє пробілові символи з початку та кінця рядка
+  if (value === ''){
+    alert('Text must not be empty')
+    return;
+  }
+  //Логіка створення елементу списку
+  const list = document.querySelector("#todo-list");
+  const li = document.createElement("li");
+  li.textContent = value;
+  list.append(li);
+
+  //Інкрементуємо лічильник завдань
+  taskCount++;
+  
+  //викликаємо метод reset - для очищення інпута після відправки форми
+  //чистимо форму після відправки
+  target.reset()
 }
 
-console.log(data);
-const cardArray = data.map((user) => {
-  return creatUserCard(user);
-});
-root.append(...cardArray);
+
