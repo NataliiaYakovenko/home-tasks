92dcdb5b45c275b2471f4a67465325bff7d13231
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-14 21:14:43 +0200

    The weather ferom Victiriia

--- index.js
+++ index.js
@@ -1,23 +1,60 @@
 "use strict";
 
-/*
-Задача 
-Написати функцію, яка через кожну секунду буде виводити числа  в консоль з 1 ло 10
-Виконати задачу циклами не можемо
-Потрібно виконанти задачу через інтервали
-Перевірити скільки секунд ми витрачаємо на виконання цієї функції
-*/
+//відобразити на сторінці поточну температуру з одиницєю виміру
+//відобразити мінусову температуру синім кольором
+// 0 - чорним
+//плюсову температуру до 40 - зеленим
+//температура >= 40 - червоним
 
-function counter(){
-  let i = 1;
-  const intervalID = setInterval(()=>{
-     console.log(i++);
-     if(i>10){
-          clearInterval(intervalID)
-          console.timeEnd('timer')
-     }
-  },1000)
+/*
+Алгоритм
+
+Обовєязкові
+1. Створити елемент
+Необхідні
+2. Задати значення атрибута
+3. Якщо необхідно.Додати класи
+4. Додати контент
+5. Додати обробник на якусь подію
+Обовєязкові
+6. Вбудуваи його туди куди потрібно
+*/
+const weatherUrl =
+  "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+
+fetch(weatherUrl)
+  .then((response) => response.json())
+  .then((data) => generateWeather(data))
+  .catch((err) => console.log("error:", err));
+
+//відобразити на сторінці поточну температуру з одиницєю виміру
+//відобразити мінусову температуру синім кольором
+// 0 - чорним
+//плюсову температуру до 40 - зеленим
+//температура >= 40 - червоним
+
+function generateWeather(data) {
+  //data.current.temperature_2m = 70 - для тестування температури
+
+  const currentTemperatureEl = document.createElement("div");
+  currentTemperatureEl.textContent = `${data.current.temperature_2m} ${data.hourly_units.temperature_2m}`;
+  currentTemperatureEl.style.color = calcTemperatureColor(data.current.temperature_2m);
+  
+  const currentWindSpeed = document.createElement("div");
+  currentWindSpeed.textContent = `${data.current.wind_speed_10m} ${data.hourly_units.wind_speed_10m}` ;
+
+  document.body.append(currentTemperatureEl);
+  document.body.append(currentWindSpeed)
 }
 
-console.time('timer') //показує скільки часу витарічаємо на виконання функції
-counter()
+function calcTemperatureColor(temperature) {
+  if (temperature < 0) {
+    return "blue";
+  } else if (temperature === 0) {
+    return "black";
+  } else if (temperature > 0 && temperature < 40) {
+    return "green";
+  } else {
+    return "red";
+  }
+}
