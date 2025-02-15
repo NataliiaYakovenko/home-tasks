218ea58051a61148d90d05dcc0b4f0608709435d
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-15 08:52:35 +0200

    Задача промісифікувати setTimeout

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 "use strict";
 
-//Задача. Кіт Шредингера
+//Промісифікувати setTimeout
+//setTimeout(cb,1000)
+//delay(1000).then(cb)
 
-function executor(resolve, reject) {
-  if (Math.random() < 0.5) {
-    resolve('the cat is alive')
-  } else {
-    reject(new Error ('the cat is dead'))
-  }
+function delay(ms){
+  function executor(resolve,reject){
+      if(typeof ms !== 'number'){
+         reject (new TypeError('milliseconds must be a number'))
+      }
+      if(ms < 0 || !Number.isInteger(ms)){
+        reject (new RangeError('milliseconds must be more 0 and must be integer'))
+      }
+       setTimeout(resolve,ms)
+     }
+
+  return new Promise(executor)
+
 }
-
-const schredingerCat = new Promise(executor);
-
-schredingerCat
-.then(data=>console.log(data))
-.catch(error=>console.log(error));
+//setTimeout(()=>console.log('action is over'),1000)
+delay(2000)
+.then(()=>console.log('action is over'))
+.catch(error => console.log(error))
