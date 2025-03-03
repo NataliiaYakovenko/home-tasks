836e343f3e88f225121d820438a6841497a6296c
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-03 13:01:37 +0200

    Add лічильник через React, інкрементує і декрементує

--- index.js
+++ index.js
@@ -1,30 +1,64 @@
-"use strict";
-class Counter extends React.Component{
-  constructor(props){
-      super(props)
+/*
+Зробіть той самий counter, який крім інкременту буде ще й декрементувати лічильник
+Лічильник не має опускатися нижчі нуля
+
+*/
+
+class Counter extends React.Component {
+    constructor(props) {
+      super(props);
       this.state = {
-          count: 0
+        count: 0,
       };
-  }
-  increment(){
-      // todo: state.count++
+    }
+    increment() {
       this.setState({
-          count: this.state.count +1
-      })
-      console.log('CLICK');
+        count: this.state.count + 1,
+      });
+    }
+  
+    decrement() {
+      if (this.state.count > 0) {
+        this.setState({
+          count: this.state.count - 1,
+        })
+      } else if (this.state.count === 0){
+          alert('Лічильник не повинен дорівнювати 0')
+      }
+    }
+  
+    render() {
+      const h2 = React.createElement("h2", {}, this.state.count);
+      const buttonIncrement = React.createElement(
+        "button",
+        {
+          onClick: () => {
+            this.increment();
+          },
+        },
+        "+"
+      );
+      const buttonDecrement = React.createElement(
+        "button",
+        {
+          onClick: () => {
+            this.decrement();
+          },
+        },
+        "-"
+      );
+      return React.createElement(
+        React.Fragment,
+        {},
+        h2,
+        buttonIncrement,
+        buttonDecrement
+      );
+    }
   }
-
-  render(){
-      const h2 = React.createElement('h2',{},this.state.count)
-      const button = React.createElement('button',{onClick: ()=>{this.increment()}}, '+')
-      return React.createElement(React.Fragment,{},h2,button);
-
-  }
-}
-const component = React.createElement(Counter)
-
-const root = document.querySelector('#root')
-ReactDOM.render(component,root)
-
-
-
+  
+  const component = React.createElement(Counter);
+  
+  const root = document.querySelector("#root");
+  
+  ReactDOM.render(component, root);
