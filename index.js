9104b75d7fdfecf2513d3895c344ce2749d9da7a
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-04 09:19:25 +0200

    Перемикач від світлого до темного

--- index.js
+++ index.js
@@ -1,64 +1,27 @@
-/*
-Зробіть той самий counter, який крім інкременту буде ще й декрементувати лічильник
-Лічильник не має опускатися нижчі нуля
+"use strict";
 
-*/
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
-class Counter extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        count: 0,
-      };
-    }
-    increment() {
-      this.setState({
-        count: this.state.count + 1,
-      });
-    }
-  
-    decrement() {
-      if (this.state.count > 0) {
-        this.setState({
-          count: this.state.count - 1,
-        })
-      } else if (this.state.count === 0){
-          alert('Лічильник не повинен дорівнювати 0')
-      }
-    }
-  
-    render() {
-      const h2 = React.createElement("h2", {}, this.state.count);
-      const buttonIncrement = React.createElement(
-        "button",
-        {
-          onClick: () => {
-            this.increment();
-          },
-        },
-        "+"
-      );
-      const buttonDecrement = React.createElement(
-        "button",
-        {
-          onClick: () => {
-            this.decrement();
-          },
-        },
-        "-"
-      );
-      return React.createElement(
-        React.Fragment,
-        {},
-        h2,
-        buttonIncrement,
-        buttonDecrement
-      );
-    }
-  }
-  
-  const component = React.createElement(Counter);
-  
-  const root = document.querySelector("#root");
-  
-  ReactDOM.render(component, root);
+function App() {
+  const isLight = true;
+  return (
+    <>
+      <ThemeButton isLightTheme={isLight} />
+    </>
+  );
+}
+
+function ThemeButton(props) {
+  const { isLightTheme } = props;
+
+  const buttonStyle = {
+    color: isLightTheme ? "blue" : "yellow",
+    backgroundColor: isLightTheme ? "yellow" : "blue",
+  };
+  return (
+    <button style={buttonStyle}>
+      Switch to {isLightTheme ? "dark" : "light"}
+    </button>
+  );
+}
